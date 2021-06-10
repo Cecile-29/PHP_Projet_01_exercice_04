45d4074f95eb7a188f72259d844a27d324ba216d
Cecile-29 <cecile.auger.pro@gmail.com>
2021-06-10 10:13:00 +0200

    push exercices PHP partie01

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP Projet01 Exercice04</title>
+</head>
+<body>
+<h1>PHP Projet01 Exercice04</h1>
+    <h2>Créez une variable age aléatoire. Afficher vous êtes majeur ou vous êtes mineur en fonction de l'âge obtenu.</h2>
+    <?php
+        $age = rand(0, 100);
+        echo $age;
+        if($age >= 18){
+            echo 'Vous êtes majeur!';
+        }else{
+            echo 'Vous êtes mineur !'
+        }
+    ?>
+</body>
+</html>
